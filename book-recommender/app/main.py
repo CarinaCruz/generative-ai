2a60b5a6a95c58abf40acd623d3de424e38aca51
CaryCCCS <carinacruzteixeira@gmail.com>
2026-03-07 15:30:10 -0300

    docker config added

--- book-recommender/app/main.py
+++ book-recommender/app/main.py
@@ -9,15 +9,14 @@ from agent import BookRecommendationAgent
 from guardrails import SecurityGuardrails
 
 guardrails = SecurityGuardrails()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # --------------------------------------------------
 # Environment & logging
 # --------------------------------------------------
 load_dotenv()
 os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 # --------------------------------------------------
 # Initialization (runs once)
@@ -29,12 +28,13 @@ def get_rag():
 
 def initialize_app():    
     rag_system = get_rag()
+    logger.info("RAG system initialized successfully.")
     llm_provider = LLMProvider()
     agent = BookRecommendationAgent(rag_system, llm_provider)
+    logger.info("Book Recommendation Agent initialized successfully.")
     return agent
 
 def run_cli(agent: BookRecommendationAgent):
-    print("📚 Book Recommendation CLI")
     question = input(">>> ")
     result = guardrails.check_user_input(question)
     if result.allowed:
@@ -43,5 +43,6 @@ def run_cli(agent: BookRecommendationAgent):
         print(answer)
 
 if __name__ == "__main__":
+    logger.info("📚 Book Recommendation CLI")
     agent = initialize_app()
     run_cli(agent)
